44c1ce5662dea66eb554efa2888607fecc1864eb
Edmund yap Keen wye <ekeen-wy@u81z05s02.42kl.edu.my>
2022-06-13 18:25:10 +0800

    made error handles

--- main.c
+++ main.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekeen-wy <ekeen-wy@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: ekeen-wy <ekeen-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 20:07:01 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2022/06/12 21:59:41 by ekeen-wy         ###   ########.fr       */
+/*   Updated: 2022/06/13 18:13:53 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	*create_array(int n)
+{
+	int	*copy;
+
+	copy = (int *)malloc(sizeof(int) * n);
+	return (copy);
+}
 
 static void	p_error(int code)
 {
@@ -24,24 +32,66 @@ static int	isnumber(char *str)
 
 	i = 0;
 	if (str[i] == 0)
-		return (0);
+		return (1);
 	if (str[i] == '-' && str[i + 1] != '\0')
 		i++;
-	else
-		return (0);
 	while (str[i] != '\0')
 	{
 		if (!ft_isdigit(str[i]))
-			return (0);
+			return (1);
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
-int	main(int argc, char**argv)
+static int isdup(int *n, size_t len)
 {
+	size_t	i;
+	size_t	j;
+	size_t	l;
+
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j <= len)
+		{
+			if (n[i] == n[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int		*array_in;
+	size_t	i;
+
 	if (argc < 2)
 		p_error(0);
-	else if (argc < 3 && isnumber == 0)
-		return ;
+	array_in = create_array(argc - 1);
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		if (isnumber(argv[i]))
+		{
+			p_error(0);
+			printf("1\n");
+			return (1);
+		}
+		else if (ft_atoi(argv[i]) > INT_MAX || ft_atoi(argv[i]) < INT_MIN)
+		{
+			p_error(0);
+			printf("2\n");
+			return (1);
+		}
+		else
+			array_in[i - 1] = ft_atoi(argv[i]);
+		i++;
+	}
+	if (isdup(array_in, argc - 1))
+		p_error(0);
 }
--- mergesort.c
+++ mergesort.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   mergesort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ekeen-wy <ekeen-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/12 20:08:20 by ekeen-wy          #+#    #+#             */
-/*   Updated: 2022/06/13 17:35:43 by ekeen-wy         ###   ########.fr       */
+/*   Created: 2022/06/13 14:09:38 by ekeen-wy          #+#    #+#             */
+/*   Updated: 2022/06/13 14:09:38 by ekeen-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <limits.h>
-# include "libft/libft.h"
-
-#endif
+void	mergesort(int *sorted, int left_index, int right_index)
+{
+	
+}
